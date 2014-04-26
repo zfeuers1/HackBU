95901a4a05baf0f69e238967f0ce50af914c03bd
Zachary Feuerstein <zackfeuerstein@gmail.com>
2014-04-25 21:40:41 -0400

    added function for box and player

--- SDL_OpenGL Template/Player.cpp
+++ SDL_OpenGL Template/Player.cpp
@@ -24,6 +24,23 @@ void Player::moveUp(){
     }
 }
 
-Player::Player(){
+Player::Player(bool whichPlayer){
+    if (whichPlayer == 0) {
+        x = 100;
+        color.R = 245;
+        color.G = 244;
+        color.B = 11;
+        color.A = 255;
+    }else{
+        x = 500;
+        color.R = 110;
+        color.G = 144;
+        color.B = 101;
+        color.A = 255;
+    }
     
+    y = 100;
+    position = 0;
+    height = 20;
+    width = 20;
 }
